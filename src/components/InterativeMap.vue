4bc903d0d55f4fef00c2e28327058f3145ee637b
Matheus William <matheuswg99@gmail.com>
2020-12-29 14:35:53 -0300

    Ajustando bug em botão Teresina

--- src/components/InterativeMap.vue
+++ src/components/InterativeMap.vue
@@ -1,252 +1,253 @@
 <template>
-    <v-img :src="require('../assets/piaui_mapa.png')" class="my-3" contain max-height="600">
-        <v-row>
-            <v-col cols="12" class="mt-2" style="margin-left: 90px;">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.parnaiba)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Parnaíba</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" class="mt-1" style="margin-left: 110px;" >
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.piripiri)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Piripiri</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" style="margin-left: 60px; margin-top: -30px">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    class="ml-5"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.barras)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Barras</strong></span>
-            </v-tooltip>
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    class="ml-2 mt-4"
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.campoMaior)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Campo Maior</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" style="margin-top: -20px;" class="ml-5">
-                <v-menu>
-                    <template v-slot:activator="{ on: menu, attrs }">
-                        <v-tooltip color="white" top>
-                            <template v-slot:activator="{ on: tooltip }">
-                                <v-btn
-                                    small
-                                    elevation="2"
-                                    fab
-                                    v-bind="attrs"
-                                    v-on="{ ...tooltip, ...menu }"
-                                >
-                                </v-btn>
-                            </template>
-                            <span style="color:  rgb(0,119,189)"><strong>Teresina</strong></span>
-                        </v-tooltip>
-                    </template>
-                    <v-list>
-                        <v-list-item
-                        v-for="(item, index) in items"
-                        :key="index"
-                        >
-                            <v-list-item-title>{{ item.title }}</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </v-col>
-            <!-- TRÍADE FLORIANO-OEIRAS-PICOS -->
-            <v-col cols="12" class="mt-8 ml-6">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.floriano)"
-                    class="ml-12"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Floriano</strong></span>
-            </v-tooltip>
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.oeiras)"
-                    class="ml-2 mt-6"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Oeiras</strong></span>
-            </v-tooltip>
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.picos)"
-                    class="ml-2"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Picos</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" style="padding-right: 320px;" class="mt-10">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.urucui)"
-                    class="ml-2"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Uruçuí</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" style="padding-right: 100px; margin-top: -10px;">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.bomJesus)"
-                    small
-                    style=" margin-right: 50px;"
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Bom Jesus</strong></span>
-            </v-tooltip>
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    style="margin-top: -30px"
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.saoRaimundoNonato)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>São Raimundo Nonato</strong></span>
-            </v-tooltip>
-            </v-col>
-            <v-col cols="12" style="padding-right: 300px;">
-            <v-tooltip color="white" top>
-                <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="changeDialogCity(citiesContents.corrente)"
-                    small
-                    elevation="2"
-                    fab
-                >
-                </v-btn>
-                </template>
-                <span style="color:  rgb(0,119,189)"><strong>Corrente</strong></span>
-            </v-tooltip>
-            </v-col>
-        </v-row>
-    </v-img>
+  <v-img
+    :src="require('../assets/piaui_mapa.png')"
+    class="my-3"
+    contain
+    max-height="600"
+  >
+    <v-row>
+      <v-col cols="12" class="mt-2" style="margin-left: 90px">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.parnaiba)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Parnaíba</strong></span>
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" class="mt-1" style="margin-left: 110px">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.piripiri)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Piripiri</strong></span>
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" style="margin-left: 60px; margin-top: -30px">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              class="ml-5"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.barras)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Barras</strong></span>
+        </v-tooltip>
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="ml-2 mt-4"
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.campoMaior)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"
+            ><strong>Campo Maior</strong></span
+          >
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" style="margin-top: -20px" class="ml-5">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="ml-2 mt-4"
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.teresina)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Teresina</strong></span>
+        </v-tooltip>
+      </v-col>
+      <!-- TRÍADE FLORIANO-OEIRAS-PICOS -->
+      <v-col cols="12" class="mt-8 ml-6">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.floriano)"
+              class="ml-12"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Floriano</strong></span>
+        </v-tooltip>
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.oeiras)"
+              class="ml-2 mt-6"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Oeiras</strong></span>
+        </v-tooltip>
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.picos)"
+              class="ml-2"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Picos</strong></span>
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" style="padding-right: 320px" class="mt-10">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.urucui)"
+              class="ml-2"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Uruçuí</strong></span>
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" style="padding-right: 100px; margin-top: -10px">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.bomJesus)"
+              small
+              style="margin-right: 50px"
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"
+            ><strong>Bom Jesus</strong></span
+          >
+        </v-tooltip>
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              style="margin-top: -30px"
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.saoRaimundoNonato)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"
+            ><strong>São Raimundo Nonato</strong></span
+          >
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12" style="padding-right: 300px">
+        <v-tooltip color="white" top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              @click="changeDialogCity(citiesContents.corrente)"
+              small
+              elevation="2"
+              fab
+            >
+            </v-btn>
+          </template>
+          <span style="color: rgb(0, 119, 189)"><strong>Corrente</strong></span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
+  </v-img>
 </template>
 
 <script>
 export default {
   data: () => ({
-      items: [
-        { title: 'Campus Poeta Torquato Neto' },
-        { title: 'Campus Clóvis Moura' },
-      ]
+    items: [
+      { title: "Campus Poeta Torquato Neto" },
+      { title: "Campus Clóvis Moura" },
+    ],
   }),
-  computed:{
-    dialog: function (){
-        return this.$store.state.main.cityDialog;
+  computed: {
+    dialog: function () {
+      return this.$store.state.main.cityDialog;
     },
-    citiesContents: function (){
-        return this.$store.state.main.citiesContents;
+    citiesContents: function () {
+      return this.$store.state.main.citiesContents;
     },
     cityDialogContent: {
-        get(){
-            return this.$store.getters.CITY_DIALOG_CONTENT;
-        },
-        set(city){
-            this.$store.commit('SET_CITY_DIALOG_CONTENT', {city: city});
-        }
+      get() {
+        return this.$store.getters.CITY_DIALOG_CONTENT;
+      },
+      set(city) {
+        this.$store.commit("SET_CITY_DIALOG_CONTENT", { city: city });
+      },
     },
   },
-  methods:{
-    changeDialogCity(city){
-      this.$store.commit('SHOW_TOGGLE_CITY_DIALOG');
+  methods: {
+    changeDialogCity(city) {
+      this.$store.commit("SHOW_TOGGLE_CITY_DIALOG");
       this.cityDialogContent = city;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  h1{
-    color: white;
-  }
-  h3{
-    color: whitesmoke;
-  }
+h1 {
+  color: white;
+}
+h3 {
+  color: whitesmoke;
+}
 </style>
